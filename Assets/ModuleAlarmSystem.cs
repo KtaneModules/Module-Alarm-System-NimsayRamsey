4c5d196d43ece0ab71ebfeabab43088d0a816541
NimsayRamsey <47064928+NimsayRamsey@users.noreply.github.com>
2022-09-22 17:31:05 -0700

    Module is now properly working

--- Assets/ModuleAlarmSystem.cs
+++ Assets/ModuleAlarmSystem.cs
@@ -10,8 +10,8 @@ using RNG = UnityEngine.Random;
 public class ModuleAlarmSystem : MonoBehaviour {
 
 	public KMBombInfo Bomb;
-	public KMNeedyModule Needy;
 	public KMAudio Audio;
+	public KMNeedyModule Needy;
 
 	public Material[] LightMat; //UnlitGreen LitGreen UnlitOrange LitOrange
 	public Renderer[] LEDs;
@@ -26,6 +26,7 @@ public class ModuleAlarmSystem : MonoBehaviour {
 	private int SubmitIndex = 0;
 	private int[] SubmitCode = {0, 0, 0, 0, 0};
 	private int[] SolveCode = {0, 0, 0, 0, 0};
+	private int SolveMODE = 0;
 	private int[,] LetterCodes = {
 		{0, 1, 0, 0, 1},
 		{1, 0, 1, 0, 1},
@@ -61,20 +62,50 @@ public class ModuleAlarmSystem : MonoBehaviour {
 		{8, 8, 8},//   8
 		{9, 19, 9}//   9
 	};
-	private string[] BLACKLIST = new string[] {};
+	private int[] invertList = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+	private string[] BOSSLIST = {
+		//Custom Additions
+		"Turn The Keys", "Custom Keys",
+		"The Swan",
+		"Silo Authorization",
+		"Scrabble Scramble",
+		//Safe and Password Modules
+		"Safety Safe",
+		"The Jewel Vault",
+		//End Solves
+		"Forget Me Not",
+		"Forget Everything",
+		"Forget This",
+		"Forget Them All",
+		"Forget Enigma",
+		"Forget Us Not",
+		"Forget Perspective",
+		"Forget Infinity"
+	};
+	private string[] BLACKLIST = {
+		"Doomsday Button",
+		"Castor",
+		"Pollux",
+		"X",
+		"Y"
+	};
 
 	private bool needyActive = false;
 	//-----------------------------------------------------//
 	
+	int moduleId;
+	static int moduleIdCounter = 1;
+
 	void Awake() {
+		moduleId = moduleIdCounter++;
 		//TRIPPED = true;
 		GetComponent<KMNeedyModule>().OnNeedyActivation += NeedyStart;
 		SOLVES = Bomb.GetSolvedModuleNames().Count();
     }
 
 	void NeedyStart() {
+		Debug.LogFormat("[ModuleAlarmSystem #{0}] Security System Engaged. Standing Idle", moduleId);
 		needyActive = true;
-		LEDs[3].material = LightMat[3];
 		LEDs[0].material = LightMat[1];
 		SegmentDisplay.text = "*****";
 		foreach (KMSelectable NAME in Keypad) {
@@ -85,7 +116,7 @@ public class ModuleAlarmSystem : MonoBehaviour {
 	}
 
 	void ModeSwitch() {
-		Audio.PlaySoundAtTransform("click26", transform);
+		Audio.PlaySoundAtTransform("click51ALT", transform);
 		LEDs[MODE].material = LightMat[0];
 		MODE += 1;
 		if (MODE == 3) {MODE = 0;}
@@ -93,10 +124,11 @@ public class ModuleAlarmSystem : MonoBehaviour {
 	}
 
 	void keypadPress(KMSelectable KEY) {
-		Audio.PlaySoundAtTransform("click25", transform);
+		Audio.PlaySoundAtTransform("click30ALT", transform);
 		int keyNum = Array.IndexOf(Keypad, KEY);
-		Debug.Log(keyNum);
+		//Debug.Log(keyNum);
 		if(TRIPPED && Tflag){
+			Audio.PlaySoundAtTransform("DigitEnter", transform);
 			SubmitCode[SubmitIndex] = keyNum;
 			SubmitIndex += 1;
 			RenderDisplay();
@@ -115,38 +147,21 @@ public class ModuleAlarmSystem : MonoBehaviour {
 		SegmentDisplay.text = REND;
 	}
 
-	void CheckSolve() {
-		bool passed = true;
-		for(int i = 0; i < 5; i++) {
-			if (SubmitCode[i] != SolveCode[i]){
-				Needy.HandleStrike();
-				passed = false;
-				break;
-			}
-		}
-
-		SubmitIndex = 0;
-		TRIPPED = false;
-		Tflag = false;
-		if (passed){
-			SegmentDisplay.text = "*****";
-		} else {
-			SegmentDisplay.text = "ERROR";
-		}
-	}
-
 	void Update() {
         if (SOLVES != Bomb.GetSolvedModuleNames().Count()) {
             GrabTrippedName();
-			Debug.Log(Tletter + " // " + MostRecent);
+			//Debug.Log(Tletter + " // " + MostRecent);
 
 			if(!BLACKLIST.Contains(MostRecent) && !TRIPPED && needyActive){
 				solveCount += 1;
 				int COIN = UnityEngine.Random.Range(0, 2);
 				if (COIN == 1 || solveCount == 3) {//COIN == 1
 					solveCount = 0;
+					TripCount += 1;
 					ModuleTripped();
 				}
+			} else {
+				Debug.LogFormat("[ModuleAlarmSystem #{0}] Module {1} is Blacklisted. Ignoring", moduleId, MostRecent);
 			}
         }
 
@@ -156,7 +171,9 @@ public class ModuleAlarmSystem : MonoBehaviour {
 	}
 
 	void ModuleTripped() {
+		Audio.PlaySoundAtTransform("Alarm2", transform);
 		string REND = "";
+		int[] TempCode = {0, 0, 0, 0, 0};
 		for (int i = 0; i < 5; i++){
 			SolveCode[i] = UnityEngine.Random.Range(0, 10);
 		}
@@ -170,10 +187,61 @@ public class ModuleAlarmSystem : MonoBehaviour {
 			SolveCode[i] += Tnumber;
 			if (SolveCode[i] > 9) {SolveCode[i] -= 10;}
 		}
-		
-		Debug.Log(SolveCode[0] + "" + SolveCode[1] + "" + SolveCode[2] + "" + SolveCode[3] + "" + SolveCode[4]);
+
+		if (BOSSLIST.Contains(MostRecent)) {
+			SolveMODE = 2;
+		} else if (CheckSerialMatch()){
+			SolveMODE = 0;
+		} else {
+			SolveMODE = 1;
+		}
+
+		if (Tnumber % 2 == 0){ //         ONE
+			TempCode[0] = SolveCode[0];
+			SolveCode[0] = SolveCode[4];
+			SolveCode[4] = TempCode[0];
+		} else { //                       TWO
+			TempCode[0] = SolveCode[1];
+			SolveCode[1] = SolveCode[3];
+			SolveCode[3] = TempCode[0];
+		}
+		if (true){
+			for (int i = 0; i < 5; i++){
+				TempCode[i] = SolveCode[i];
+			}
+			SolveCode[0] = TempCode[3];
+			SolveCode[1] = TempCode[4];
+			SolveCode[2] = TempCode[0];
+			SolveCode[3] = TempCode[1];
+			SolveCode[4] = TempCode[2];
+		}
+		if (TripCount % 2 == 0){ //       FOUR
+			TempCode[0] = SolveCode[0];
+			TempCode[1] = SolveCode[1];
+			SolveCode[0] = SolveCode[4];
+			SolveCode[1] = SolveCode[3];
+			SolveCode[4] = TempCode[0];
+			SolveCode[3] = TempCode[1];
+		}
+
+		if (SolveMODE == 2){ //           FIVE
+			for (int i = 0; i < 5; i++){
+				SolveCode[i] = invertList[SolveCode[i]];
+			}
+		}
+
+		Debug.LogFormat("[ModuleAlarmSystem #{0}] [ALARM TRIPPED BY {1}] Number of trips is [{2}] TRIP letter is [{3}] Display showing [{4}]", moduleId, MostRecent, TripCount, Tletter, REND);
+		Debug.LogFormat("[ModuleAlarmSystem #{0}] Solution Code [{1}{2}{3}{4}{5}] on MODE [{6}]", moduleId, SolveCode[0], SolveCode[1], SolveCode[2], SolveCode[3], SolveCode[4], SolveMODE+1);
 		SegmentDisplay.text = REND;
 		TRIPPED = true;
+	}
+
+	bool CheckSerialMatch() {
+		string serialNum = Bomb.GetSerialNumber();
+		for (int i = 0; i < 6; i++){
+			if (serialNum[i].ToString() == Tletter){return true;}
+		}
+		return false;
 	}
 
 	void GrabTrippedName () { //Borrowed from Validation
@@ -188,7 +256,11 @@ public class ModuleAlarmSystem : MonoBehaviour {
   	        	module = module.Substring(4);
         	}
 			Tletter = module.Substring(0, 1).ToUpper();
-			Tnumber = AlphabetVAL[Array.IndexOf(AlphabetLET, Tletter)];
+			if(Regex.IsMatch(Tletter, "[0-9]")){
+				Tnumber = Int32.Parse(Tletter);
+			} else {
+				Tnumber = AlphabetVAL[Array.IndexOf(AlphabetLET, Tletter)];
+			}
 		}
 	}
 
@@ -208,17 +280,43 @@ public class ModuleAlarmSystem : MonoBehaviour {
 	void TripCheck() {
 		//Debug.Log(Needy.GetNeedyTimeRemaining());
 		if (TRIPPED && !Tflag){
-			Needy.SetNeedyTimeRemaining(60);
+			Needy.SetNeedyTimeRemaining(120);
 			Tflag = true;
-			TripCount += 1;
+			LEDs[3].material = LightMat[3];
 		} else if (!TRIPPED) { //Needy.GetNeedyTimeRemaining() != SelectedNumber
 			Needy.SetNeedyTimeRemaining(88);
+			LEDs[3].material = LightMat[2];
 		} else if (Needy.GetNeedyTimeRemaining() < 0.5f) {
 			Needy.HandleStrike();
 			SubmitIndex = 0;
 			TRIPPED = false;
 			Tflag = false;
 			SegmentDisplay.text = "ERROR";
+			Debug.LogFormat("[ModuleAlarmSystem #{0}] Module Striked [Time Depleted] // Returning to Idle", moduleId);
 		}
+	}
+
+	void CheckSolve() {
+		bool passed = true;
+		for(int i = 0; i < 5; i++) {
+			if (SubmitCode[i] != SolveCode[i] || MODE != SolveMODE){
+				Needy.HandleStrike();
+				passed = false;
+				break;
+			}
+		}
+
+		SubmitIndex = 0;
+		TRIPPED = false;
+		Tflag = false;
+		if (passed){
+			Audio.PlaySoundAtTransform("Confirm1", transform);
+			SegmentDisplay.text = "*****";
+			Debug.LogFormat("[ModuleAlarmSystem #{0}] Submitted Code [{1}{2}{3}{4}{5}] on MODE [{6}] // Code Accepted", moduleId, SubmitCode[0], SubmitCode[1], SubmitCode[2], SubmitCode[3], SubmitCode[4], MODE+1);
+		} else {
+			SegmentDisplay.text = "ERROR";
+			Debug.LogFormat("[ModuleAlarmSystem #{0}] Submitted Code [{1}{2}{3}{4}{5}] on MODE [{6}] // Incorrect Code [Module Striked]", moduleId, SubmitCode[0], SubmitCode[1], SubmitCode[2], SubmitCode[3], SubmitCode[4], MODE+1);
+		}
+		Debug.LogFormat("[ModuleAlarmSystem #{0}] Returning to Idle", moduleId);
 	}
 }
